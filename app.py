c524126991b3f1885e7f8c189be948b70869a773
Brendan Murphy <thesimplekid@brendanmurphy.xyz>
2020-09-11 17:02:06 -0400

    populates db

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ logging.warning("New Run Starts Here")
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(routes_for_flask)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://user:pavgncbrs1wvrd0r@db-mysql-nyc1-43883-do-user-7668124-0.b.db.ondigitalocean.com:25060/defaultdb?"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://user:wd058h62upaogl6t@db-mysql-nyc1-23535-do-user-7668124-0.b.db.ondigitalocean.com:25060/defaultdbone?"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # Shows sql querys being made if having database issue set to true
     app.config['SQLALCHEMY_ECHO'] = True
@@ -24,4 +24,5 @@ if __name__ == "__main__":
     app = create_app()
     app.app_context().push()
     models.db.create_all()
+    models.populate()
     app.run()
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from dataclasses import asdict, dataclass
 from datetime import datetime
 
@@ -23,9 +24,16 @@ class Student(db.Model):
     schoolDBN: str = db.Column(db.String(80), nullable=False)
 
     student_iep_mandate = db.relationship(
-        'IEP_Mandate', back_populates='student')
-    rules = db.relationship('Rule', back_populates='student')
-    sessions = db.relationship('Session', back_populates='student')
+        'Iepmandate', back_populates='student')
+
+    '''
+    student_ = db.relationship(
+        'Iepmandate', back_populates='student')
+        '''
+    student_rules = db.relationship('Rule', back_populates='student')
+
+    student_sessions = db.relationship(
+        'Session', back_populates='student_in_session')
 
 
 @dataclass
@@ -36,11 +44,11 @@ class Therapist(db.Model):
     first_name: str = db.Column(db.String(120), nullable=False)
     last_name: str = db.Column(db.String(120), nullable=False)
 
-    rules = db.relationship('Rule', back_populates='therapist')
+    therapist_rules = db.relationship('Rule', back_populates='therapist')
 
 
 @dataclass
-class IepMandate(db.Model):
+class Iepmandate(db.Model):
 
     iep_id: int = db.Column(
         db.Integer, primary_key=True, autoincrement=True)
@@ -58,7 +66,7 @@ class IepMandate(db.Model):
         'student.student_id'), nullable=False)
     student = db.relationship("Student", back_populates='student_iep_mandate')
 
-    rules = db.relationship('Rule', back_populates='iep_mandate')
+    iep_rules = db.relationship('Rule', back_populates='ieps')
 
 
 @dataclass
@@ -88,20 +96,20 @@ class Rule(db.Model):
 
     therapist_id: int = db.Column(db.Integer, db.ForeignKey(
         'therapist.therapist_id'), nullable=False)
-    therapist = db.relationship("Therapist", back_populates='rules')
+    therapist = db.relationship("Therapist", back_populates='therapist_rules')
 
     iep_id: int = db.Column(db.Integer, db.ForeignKey(
-        'iep_mandate.iep_id'), nullable=False)
-    ieps = db.relationship("IEP_Mandate", back_populates='rules')
+        'iepmandate.iep_id'), nullable=False)
+    ieps = db.relationship("Iepmandate", back_populates='iep_rules')
 
     student_id: int = db.Column(db.Integer, db.ForeignKey(
         'student.student_id'), nullable=False)
-    student = db.relationship("Student", back_populates='rules')
+    student = db.relationship("Student", back_populates='student_rules')
 
-    sessions = db.relationship('session', back_populates='student')
+    rule_sessions = db.relationship('Session', back_populates='rule')
 
 
-@dataclass
+@ dataclass
 class Session(db.Model):
     session_id = db.Column(
         db.Integer, primary_key=True, autoincrement=True)
@@ -115,12 +123,70 @@ class Session(db.Model):
     attended: bool = db.Column(db.Boolean, nullable=False)
     notes: str = db.Column(db.String(500), nullable=True)
 
-    # TODO: student id
     student_id: int = db.Column(db.Integer, db.ForeignKey(
         'student.student_id'), nullable=False)
-    student = db.relationship("Student", back_populates='sessions')
-    # TODO: rule id
+    student_in_session = db.relationship(
+        "Student", back_populates='student_sessions')
 
     rule_id: int = db.Column(db.Integer, db.ForeignKey(
         'rule.rule_id'), nullable=False)
-    rule = db.relationship("Rule", back_populates='sessions')
+    rule = db.relationship("Rule", back_populates='rule_sessions')
+
+
+def insert_student(osis_number_passed, first_name_passed, last_name_passed, grade_passed, schoolDBN_passed):
+    new_student = Student(osis_number=osis_number_passed, first_name=first_name_passed,
+                          last_name=last_name_passed, grade=grade_passed, schoolDBN=schoolDBN_passed)
+    db.session.add(new_student)
+    db.session.commit()
+
+
+def insert_therapist(therapist_ref_id_passed, first_name_passed, last_name_passed):
+    new_therapist = Therapist(therapist_ref_id=therapist_ref_id_passed,
+                              first_name=first_name_passed, last_name=last_name_passed)
+    db.session.add(new_therapist)
+    db.session.commit()
+
+
+def insert_iepmandate(frequency_passed, duration_passed, group_size_passed, type_passed, student_id_passed):
+    new_iep = Iepmandate(frequency=frequency_passed, duration=duration_passed,
+                         group_size=group_size_passed, type=type_passed, student_id=student_id_passed)
+    db.session.add(new_iep)
+    db.session.commit()
+
+
+def insert_rule(location_passed, interval_passed, day_passed, repeats_passed, start_date_passed, end_date_passed, start_time_passed, therapist_id_passed, iep_id_passed, student_id_passed):
+    new_rule = Rule(location=location_passed, interval=interval_passed, day=day_passed, repeats=repeats_passed, start_date=start_date_passed,
+                    end_date=end_date_passed, start_time=start_time_passed, therapist_id=therapist_id_passed, iep_id=iep_id_passed, student_id=student_id_passed)
+    db.session.add(new_rule)
+    db.session.commit()
+
+
+def insert_session(date_passed, start_time_passed, end_time_passed, duration_passed, attended_passed, notes_passed, student_id_passed, rule_id_passed):
+    new_session = Session(date=date_passed, start_time=start_time_passed, end_time=end_time_passed, duration=duration_passed,
+                          attended=attended_passed, notes=notes_passed, student_id=student_id_passed, rule_id=rule_id_passed)
+    db.session.add(new_session)
+    db.session.commit()
+
+
+def populate():
+    insert_student(88, 'Jack', 'Murphy', 'pre-k', '127Q2')
+    insert_student(99, 'Brendan', 'Murphy', '1', '9674q')
+
+    insert_therapist(74783, 'Caroline', 'Murphy')
+    insert_therapist(7646783, "tom", 'Murphy')
+
+    insert_iepmandate(5, 65, 2, 'speech', 1)
+    insert_iepmandate(2, 45, 1, 'PT', 2)
+    currentDT = datetime.now()
+    timers = currentDT.strftime("%H:%M")
+
+    insert_rule('in-person', 'weekly', 'Monday', 2, datetime(2020,
+                                                             5, 17), datetime(2020, 6, 17), timers, 1, 1, 1)
+
+    insert_rule('teletheraphy', 'daily', 'Tuesday', 2, datetime(
+        2020, 6, 18), datetime(2020, 7, 17), timers, 2, 2, 2)
+
+    insert_session(datetime(2020, 7, 17), timers,
+                   timers, 77, True, 'hi', 1, 1)
+    insert_session(datetime(2020, 7, 18), timers,
+                   timers, 77, False, 'hi', 2, 2)
